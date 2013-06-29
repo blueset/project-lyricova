af219ba2b4c27edc164ee7ad8877c62256ad2a32
Blueset <ilove@1a23.com>
2013-06-29 22:46:14 +0800

    Beta 0.1 build 20130629

--- application/config/version.php
+++ application/config/version.php
@@ -1,12 +1,4 @@
  <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
  //Version information.
-<<<<<<< HEAD
-<<<<<<< HEAD
  $config['current_version'] = 'Beta 0.1 build 20130629';
-=======
- $config['current_version'] = 'Beta 0.1 build 20130626';
->>>>>>> d1f1a03c0602f2fa54a4ec1bf238c0cb07d83123
-=======
- $config['current_version'] = 'Beta 0.1 build 20130626';
->>>>>>> d1f1a03c0602f2fa54a4ec1bf238c0cb07d83123
