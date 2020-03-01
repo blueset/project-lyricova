72ff7e2b2149bc4f1de95e4f84a125547f4639cd
Eana Hufwe <ilove@1a23.com>
2020-03-01 21:56:18 +0800

    feat(jukebox): add sort order for file in playlist

--- packages/jukebox/src/models/FileInPlaylist.ts
+++ packages/jukebox/src/models/FileInPlaylist.ts
@@ -1,6 +1,6 @@
 import { MusicFile } from "./MusicFile";
 import { DataTypes } from "sequelize";
-import { Model, CreatedAt, UpdatedAt, DeletedAt, Column, Table, PrimaryKey, ForeignKey, AutoIncrement } from "sequelize-typescript";
+import { Model, CreatedAt, UpdatedAt, Column, Table, PrimaryKey, ForeignKey, AutoIncrement } from "sequelize-typescript";
 import { Playlist } from "./Playlist";
 
 @Table
@@ -18,6 +18,9 @@ export class FileInPlaylist extends Model<FileInPlaylist> {
   @ForeignKey(() => Playlist)
   @Column
   playlistId: number;
+
+  @Column({ type: new DataTypes.INTEGER, defaultValue: 0 })
+  sortOrder: number;
 
   @CreatedAt
   creationDate: Date;
