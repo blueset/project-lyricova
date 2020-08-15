2bcb03014d5dd209a358ad675dc615cfce213d16
Eana Hufwe <ilove@1a23.com>
2020-08-15 22:32:24 +0800

    wip(jukebox): static lyrics animation

--- packages/jukebox/src/components/public/lyrics/static.tsx
+++ packages/jukebox/src/components/public/lyrics/static.tsx
@@ -3,32 +3,43 @@ import { useAppContext } from "../AppContext";
 import { useLyricsState } from "../../../frontendUtils/hooks";
 import { Box, makeStyles, Theme } from "@material-ui/core";
 import { blendStyleProperties, BlendStyleParams } from "../../../frontendUtils/blendStyle";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
-const useStyle = makeStyles<Theme, BlendStyleParams>({
-  currentLine: {
-    fontSize: "4.5em",
-    fontWeight: 600,
-    marginBottom: 32,
-    lineHeight: 1.2,
-    transition: "font-size 0.25s",
-    ...blendStyleProperties(),
-    "& > small": {
-      display: "block",
-      fontSize: "0.6em",
+const useStyle = makeStyles<Theme, BlendStyleParams>((theme) => {
+  return {
+    container: {
+      padding: theme.spacing(4),
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    currentLine: {
+      fontSize: "4.5em",
+      fontWeight: 600,
+      marginBottom: theme.spacing(6),
+      lineHeight: 1.2,
+      transition: "font-size 0.05s ease-in-out",
+      ...blendStyleProperties(),
+      "& > small": {
+        display: "block",
+        fontSize: "0.6em",
+      }
+    },
+    nextLine: {
+      fontSize: "2.5em",
+      fontWeight: 600,
+      color: "rgba(255, 255, 255, 0.3)",
+      lineHeight: 1.2,
+      transition: "font-size 0.05s ease-in-out",
+      ...blendStyleProperties({ filterName: "#sharpBlurBright" }),
+      "& > small": {
+        display: "block",
+        fontSize: "0.8em",
+      }
     }
-  },
-  nextLine: {
-    fontSize: "2.5em",
-    fontWeight: 600,
-    color: "rgba(255, 255, 255, 0.3)",
-    lineHeight: 1.2,
-    transition: "font-size 0.25s",
-    ...blendStyleProperties({ filterName: "#sharpBlurBright" }),
-    "& > small": {
-      display: "block",
-      fontSize: "0.8em",
-    }
-  }
+  };
 });
 
 interface Props {
@@ -43,12 +54,16 @@ interface LyricsLineElementProps {
 
 function LyricsLineElement({ className, line }: LyricsLineElementProps) {
   if (!line) return null;
-  return (<div className={className} lang="ja">
-    {line.content}
-    {line.attachments?.translation && (
-      <small lang="zh">{line.attachments.translation}</small>
-    )}
-  </div>);
+  return (
+    <AnimatePresence>
+      <motion.div layout className={className} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut" }} exit={{ opacity: 0 }} lang="ja">
+        {line.content}
+        {line.attachments?.translation && (
+          <small lang="zh">{line.attachments.translation}</small>
+        )}
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
 export function StaticLyrics({ lyrics }: Props) {
@@ -60,10 +75,14 @@ export function StaticLyrics({ lyrics }: Props) {
   const lines = lyrics.lines;
 
 
-  return (<Box padding={4} width={1} height={1} display="flex" flexDirection="column" justifyContent="center">
-    {line !== null && lines.map((l, idx) => {
-      if (idx < line || idx > line + 1) return null;
-      return <LyricsLineElement className={idx === line ? styles.currentLine : styles.nextLine} line={l} key={idx} />;
-    })}
-  </Box>);
+  return (
+    <AnimateSharedLayout>
+      <motion.div layout className={styles.container}>
+        {line !== null && lines.map((l, idx) => {
+          if (idx < line || idx > line + 1) return null;
+          return <LyricsLineElement className={idx === line ? styles.currentLine : styles.nextLine} line={l} key={idx} />;
+        })}
+      </motion.div>
+    </AnimateSharedLayout>
+  );
 }
