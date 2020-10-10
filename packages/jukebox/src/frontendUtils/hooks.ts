a3f61e6e5017254e733c462438bab697edcc69b0
Eana Hufwe <ilove@1a23.com>
2020-10-10 23:43:06 +0800

    feat(jukebox): implement keyframe based player-lyrics state hook
    Inspired by LyricsKit by @ddddxxx

--- packages/jukebox/src/frontendUtils/hooks.ts
+++ packages/jukebox/src/frontendUtils/hooks.ts
@@ -1,8 +1,9 @@
 import { useState, useDebugValue, useEffect, RefObject, useCallback, useRef, useMemo } from "react";
-import { LyricsKitLyrics } from "../graphql/LyricsKitObjects";
+import { LyricsKitLyrics, LyricsKitLyricsLine } from "../graphql/LyricsKitObjects";
 import _ from "lodash";
 import { AnimatedWord } from "../utils/typingSequence";
 import { gql, QueryResult, useQuery } from "@apollo/client";
+import { Simulate } from "react-dom/test-utils";
 
 
 export function useNamedState<T>(initialValue: T, name: string) {
@@ -90,6 +91,167 @@ export function useLyricsState(playerRef: RefObject<HTMLAudioElement>, lyrics: L
 
   return line;
 }
+
+// region Refactored keyframe-based lyrics state
+
+export type PlayerState = {
+  state: "playing";
+  /**
+   * `performance.now()` formatted number (millisecond) adjusted the progress music.
+   *
+   * e.g. `performance.now() - (player.currentTime * 1000)`.
+   */
+  startingAt: number;
+} | {
+  state: "paused";
+  /** Progress of the current player (seconds). */
+  progress: number;
+};
+
+export interface PlayerLyricsKeyframe<T> {
+  /** Start of the frame, in seconds. */
+  start: number;
+  data: T;
+}
+
+export interface PlayerLyricsState<T> {
+  playerState: PlayerState;
+  /**
+   * Current frame can be anything in [-1, keyframe.length - 1].
+   * -1 means before the first frame starts. Final frame lasts forever.
+   */
+  currentFrameId: number;
+  /** The current frame content. `null` when frame ID is -1. */
+  currentFrame: null | PlayerLyricsKeyframe<T>;
+  /** End time of current frame (in seconds). End of track for the last frame. */
+  endTime: number;
+  /** `startTimes[i]` is when frame `i` starts, in seconds. */
+  startTimes: number[];
+  /**
+   * `endTimes[i + 1]` is when frame `i` ends, in seconds.
+   * Last value is the end of the track.
+   */
+  endTimes: number[];
+}
+
+
+export function usePlayerLyricsState<T>(keyframes: PlayerLyricsKeyframe<T>[], playerRef: RefObject<HTMLAudioElement>): PlayerLyricsState<T> {
+  const [playerState, setPlayerState] = useNamedState<PlayerState>({ state: "paused", progress: 0 }, "playerState");
+  const playerStateRef = useRef<PlayerState>();
+  playerStateRef.current = playerState;
+  const frameCallbackRef = useRef<number>();
+
+  // Added to prevent operation on unmounted components
+  const thisLifespan = useRef<boolean>();
+
+  /**
+   * Current frame can be anything in [-1, keyframe.length - 1].
+   * -1 means before the first frame starts. Final frame lasts forever.
+   */
+  const [currentFrameId, setCurrentFrameId] = useNamedState(-1, "currentFrameId");
+  const currentFrameIdRef = useRef<number>();
+  currentFrameIdRef.current = currentFrameId;
+
+  /**
+   * `endTimes[i + 1]` is when frame `i` ends, in seconds.
+   * Last value is the end of the track.
+   */
+  const endTimes = useMemo(() => {
+    const endTimes = [];
+    keyframes.forEach((v, idx) => {
+      endTimes[idx] = v.start;
+    });
+
+    if (playerRef.current) endTimes[keyframes.length] = playerRef.current.duration;
+    endTimes[keyframes.length] = keyframes[keyframes.length - 1].start + 10;
+
+    return endTimes;
+  }, [keyframes, playerRef]);
+
+  const startTimes = useMemo(() => keyframes.map(v => v.start), [keyframes]);
+
+  const onFrame = useCallback((timestamp: number) => {
+    // Out of current lifespan, stop.
+    if (!thisLifespan.current) return;
+
+    const playerState = playerStateRef.current;
+    const currentFrameId = currentFrameIdRef.current;
+    const end = endTimes[currentFrameId + 1];
+
+    let time;
+    if (playerState.state === "paused") {
+      time = playerState.progress;
+    } else {
+      time = (timestamp - playerState.startingAt) / 1000;
+    }
+
+    if (time > end) {
+      setCurrentFrameId(currentFrameId + 1);
+    }
+
+    if (playerState.state === "playing") {
+      frameCallbackRef.current = requestAnimationFrame(onFrame);
+    }
+  }, [endTimes, setCurrentFrameId]);
+
+  const updatePlayerState = useCallback(() => {
+    const player = playerRef.current;
+    if (!player) return;
+    if (player.paused) {
+      setPlayerState({ state: "paused", progress: player.currentTime });
+    } else {
+      setPlayerState({ state: "playing", startingAt: performance.now() - (player.currentTime * 1000) });
+    }
+    // Clear existing loop.
+    if (frameCallbackRef.current !== null) {
+      cancelAnimationFrame(frameCallbackRef.current);
+    }
+    // Find current frame
+    setCurrentFrameId(_.sortedLastIndex(startTimes, player.currentTime) - 1);
+
+    // Start a new loop.
+    frameCallbackRef.current = requestAnimationFrame(onFrame);
+  }, [onFrame, playerRef, setCurrentFrameId, setPlayerState, startTimes]);
+
+  // Register event listeners
+  useEffect(() => {
+    thisLifespan.current = true;
+
+    const player = playerRef.current;
+    if (!player) return;
+
+    player.addEventListener("play", updatePlayerState);
+    player.addEventListener("pause", updatePlayerState);
+    player.addEventListener("seeked", updatePlayerState);
+    updatePlayerState();
+
+    return () => {
+      thisLifespan.current = false;
+      player.removeEventListener("play", updatePlayerState);
+      player.removeEventListener("pause", updatePlayerState);
+      player.removeEventListener("seeked", updatePlayerState);
+    };
+  }, [playerRef, updatePlayerState]);
+
+  return {
+    playerState,
+    currentFrameId,
+    currentFrame: currentFrameId >= 0 ? keyframes[currentFrameId] : null,
+    endTime: endTimes[currentFrameId + 1],
+    startTimes,
+    endTimes,
+  };
+}
+
+export function usePlainPlayerLyricsState(lyrics: LyricsKitLyrics, playerRef: RefObject<HTMLAudioElement>): PlayerLyricsState<LyricsKitLyricsLine> {
+  const keyFrames: PlayerLyricsKeyframe<LyricsKitLyricsLine>[] = useMemo(() => lyrics.lines.map(v => ({
+    start: v.position,
+    data: v
+  })), [lyrics]);
+  return usePlayerLyricsState<LyricsKitLyricsLine>(keyFrames, playerRef);
+}
+
+// endregion Refactored keyframe-based lyrics state
 
 const SEQUENCE_QUERY = gql`
   query TypingSequence($text: String!) {
