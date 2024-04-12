748f14bef3e1c20576c5a90bd5c2ea781d60d9bc
eana <coder@apps.1a23.com>
2024-04-12 00:17:47 +0000

    monoruby

---
+++ packages/jukebox/src/utils/monoruby.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "@jest/globals";
+import { convertMonoruby } from "./monoruby";
+
+describe("convertMonoruby", () => {
+    it("should convert monoruby properly", () => {
+        expect(convertMonoruby("漢字", "かんじ")).toEqual([["漢", "字"], ["かん", "じ"]]);
+        expect(convertMonoruby("他人事", "たにんごと")).toEqual([["他", "人", "事"], ["た", "にん", "ごと"]]);
+        expect(convertMonoruby("他力本願", "たりきほんがん")).toEqual([["他", "力", "本", "願"], ["た", "りき", "ほん", "がん"]]);
+        expect(convertMonoruby("同人音楽", "どうじんおんがく")).toEqual([["同", "人", "音", "楽"], ["どう", "じん", "おん", "がく"]]);
+        expect(convertMonoruby("大義名分", "たいぎめいぶん")).toEqual([["大", "義", "名", "分"], ["たい", "ぎ", "めい", "ぶん"]]);
+        expect(convertMonoruby("宇田川町", "うだがわちょう")).toEqual([["宇", "田", "川", "町"], ["う", "だ", "がわ", "ちょう"]]);
+        expect(convertMonoruby("抱きしめて!", "だきしめて")).toEqual([["抱", "き", "し", "め", "て", "!"], ["だ", "き", "し", "め", "て", ""]]);
+        expect(convertMonoruby("日向電工", "ひなたでんこう")).toEqual([["日向電工"], ["ひなたでんこう"]]);
+        expect(convertMonoruby("日進月歩", "にっしんげっぽ")).toEqual([["日", "進", "月", "歩"], ["にっ", "しん", "げっ", "ぽ"]]);
+    });
+});
---
+++ packages/jukebox/src/utils/monoruby.ts
@@ -0,0 +1,64 @@
+import data from "./kanjidic2.mod.min.json";
+import { kanaToHira } from "lyricova-common/utils/kanaUtils";
+
+const mapping: { [key: string]: string[] } = data;
+
+const dakuon = {
+    "か": "が", "き": "ぎ", "く": "ぐ", "け": "げ", "こ": "ご",
+    "さ": "ざ", "し": "じ", "す": "ず", "せ": "ぜ", "そ": "ぞ",
+    "た": "だ", "ち": "ぢ", "つ": "づ", "て": "で", "と": "ど",
+    "は": "ば", "ひ": "び", "ふ": "ぶ", "へ": "べ", "ほ": "ぼ",
+};
+
+const handauon = {
+    "は": "ぱ", "ひ": "ぴ", "ふ": "ぷ", "へ": "ぺ", "ほ": "ぽ",
+};
+
+function patchReading(readings: string[], base: string): string[] {
+    const result: string[] = [];
+    for (const reading of readings) {
+        result.push(reading);
+        const hira = kanaToHira(reading);
+        // katakana to hiragana
+        if (hira !== reading) {
+            result.push(hira);
+        }
+        if (hira.match(/[つくち]$/)) {
+            result.push(hira.replace(/[つくち]$/, "っ"));
+        }
+        if (hira.match(/^[かきくけこさしすせそたちつてと]/)) {
+            result.push(hira.replace(/^([かきくけこさしすせそたちつてとはひふへほ])/, (m: keyof typeof dakuon) => dakuon[m]));
+        }
+        if (hira.match(/^[はひふへほ]/)) {
+            result.push(hira.replace(/^([はひふへほ])/, (m: keyof typeof handauon) => handauon[m]));
+        }
+    }
+    // match base itself
+    result.push(base);
+    // match nothing
+    result.push("");
+    return result;
+}
+
+export function convertMonoruby(base: string, reading: string): [string[], string[]] {
+    const baseGroups: string[] = [];
+    const readingGroups: string[] = [];
+    let i = 0;
+
+    for (const baseChar of base) {
+        for (const readingCandid of patchReading(mapping[baseChar] ?? [], baseChar)) {
+            if (reading.startsWith(readingCandid, i)) {
+                baseGroups.push(baseChar);
+                readingGroups.push(readingCandid);
+                i += readingCandid.length;
+                break;
+            }
+        }
+    }
+
+    if (i < reading.length) {
+        return [[base], [reading]];
+    }
+
+    return [baseGroups, readingGroups];
+}
