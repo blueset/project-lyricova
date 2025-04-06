b26b2d993753306b94f9d2ca567de040f97194d4
Eana Hufwe <eana@1a23.com>
2025-04-06 16:05:50 +0000

    Drop common package

--- packages/common/src/admin/commands/user.ts
+++
@@ -1,9 +0,0 @@
-import type yargs from "yargs";
-
-export const command = "user";
-export const desc = "Manage users";
-export const builder = (yargs: yargs.Argv) => {
-  return yargs.commandDir("user").demandCommand();
-};
-export const handler = (argv: yargs.Argv) => {
-};
