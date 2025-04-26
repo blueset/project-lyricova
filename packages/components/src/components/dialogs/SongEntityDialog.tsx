631c6e545e039190f7ce5645f81b956aa59f6124
Eana Hufwe <eana@1a23.com>
2025-04-26 02:40:37 +0000

    feat(jukebox): fix form validation

--- packages/components/src/components/dialogs/SongEntityDialog.tsx
+++ packages/components/src/components/dialogs/SongEntityDialog.tsx
@@ -89,7 +89,7 @@ const formSchema = z.object({
         .refine((val) => val !== null, "Artist entity must be selected"),
       artistRoles: z.array(z.string()),
       categories: z.array(z.string()),
-      customName: z.string().optional(),
+      customName: z.string().optional().or(z.literal("")).or(z.null()),
       isSupport: z.boolean(),
     })
   ),
@@ -99,7 +99,13 @@ const formSchema = z.object({
         .any()
         .refine((val) => val !== null, "Album entity must be selected"),
       diskNumber: z.number().positive().int().optional(),
-      trackNumber: z.number().positive().int().optional(),
+      trackNumber: z
+        .number()
+        .positive()
+        .int()
+        .optional()
+        .or(z.literal(""))
+        .or(z.null()),
       name: z.string().min(1, "Required"),
     })
   ),
