d9500d9684dae8284bdae0a3e3dc81f2663e5b4c
Eana Hufwe <eana@1a23.com>
2025-05-24 01:48:37 +0000

    fix(jukebox): sonner toast expand

--- packages/components/src/components/ui/sonner.tsx
+++ packages/components/src/components/ui/sonner.tsx
@@ -1,13 +1,14 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      expand
       style={
         {
           "--normal-bg": "var(--popover)",
@@ -17,7 +18,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
